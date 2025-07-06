62517471a47b11678fa99e75e388826a4590729d
上川雅弘 <k2210182@gl.cc.uec.ac.jp>
2025-07-06 15:37:50 +0900

    commandのattribute(属性、オブジェクトや要素に付加される追加情報)をつける

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
+#[tauri::command]
+fn simple_command() {
+    println!("I was invoked from JS!");
+}
 fn main() {
     WiFiAnalyzer2nd_lib::run()
 }
